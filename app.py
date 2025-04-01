c77cab3a92706c8431df582896031236cb3da27d
John1 <John1@hmanage.xyz>
2025-04-01 18:54:08 +0800

    update

--- app.py
+++ app.py
@@ -4,37 +4,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    html = render_template("index.html")
-    with open("static/index.html", "w", encoding="utf-8") as file:
-        file.write(html)
-    return html
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    html = render_template("layouts/about.html")  # Adjusted to your directory structure
-    with open("static/about.html", "w", encoding="utf-8") as file:
-        file.write(html)
-    return html
+    return render_template("layouts/about.html")
 
 @app.route("/education")
 def education():
-    html = render_template("layouts/education.html")
-    with open("static/education.html", "w", encoding="utf-8") as file:
-        file.write(html)
-    return html
+    return render_template("layouts/education.html")
+
 @app.route("/skills")
 def skills():
-    html = render_template("layouts/skills.html")
-    with open("static/skills.html", "w", encoding="utf-8") as file:
-        file.write(html)
-    return html
+    return render_template("layouts/skills.html")
 
 @app.route("/work")
 def work():
-    html = render_template("layouts/work.html")
-    with open("static/work.html", "w", encoding="utf-8") as file:
-        file.write(html)
-    return html
+    return render_template("layouts/work.html")
+    
+@app.route("/contact")
+def contact():
+    return render_template("layouts/contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
