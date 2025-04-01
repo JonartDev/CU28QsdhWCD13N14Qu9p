51bf6b8551f3fb6014d368c9cad7c82331caa370
John1 <John1@hmanage.xyz>
2025-04-01 18:45:39 +0800

    update

--- app.py
+++ app.py
@@ -4,7 +4,37 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    html = render_template("index.html")
+    with open("static/index.html", "w", encoding="utf-8") as file:
+        file.write(html)
+    return html
+
+@app.route("/about")
+def about():
+    html = render_template("layouts/about.html")  # Adjusted to your directory structure
+    with open("static/about.html", "w", encoding="utf-8") as file:
+        file.write(html)
+    return html
+
+@app.route("/education")
+def education():
+    html = render_template("layouts/education.html")
+    with open("static/education.html", "w", encoding="utf-8") as file:
+        file.write(html)
+    return html
+@app.route("/skills")
+def skills():
+    html = render_template("layouts/skills.html")
+    with open("static/skills.html", "w", encoding="utf-8") as file:
+        file.write(html)
+    return html
+
+@app.route("/work")
+def work():
+    html = render_template("layouts/work.html")
+    with open("static/work.html", "w", encoding="utf-8") as file:
+        file.write(html)
+    return html
 
 if __name__ == "__main__":
     app.run(debug=True)
