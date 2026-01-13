e615545e5244c4e2fbe51fc66b93e3af29ff59f0
John1 <John1@hmanage.xyz>
2026-01-13 16:56:07 +0800

    update resume

--- assets/js/download_cv.js
+++ assets/js/download_cv.js
@@ -1,8 +1,8 @@
 
 function downloadFile() {
     const link = document.createElement('a');
-    link.href = 'assets/cv_resume.pdf'; // Ensure this path is correct
-    link.download = 'cv_resume.pdf'; // Set the filename for download
+    link.href = 'assets/JonartOlicia.pdf'; // Ensure this path is correct
+    link.download = 'JonartOlicia.pdf'; // Set the filename for download
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
